2ab64c29170a178234b265a5f4d551328e929de8
hyekyung Kim <ilovacom88@naver.com>
2024-07-23 23:31:41 +0900

    first commit

---
+++ react-router-dom-example/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+import styled from 'styled-components'
+const SimpleButton = styled.button``;
+
+export default function App{
+  return(
+    <div> 
+    <SimpleButton>Simpke</SimpleButton> 
+    </div>
+  );
+}
